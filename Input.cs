f79016955bd377d8432ede8b34d40e23d0eb05c7
FilipMagpie99 <59532530+FilipMagpie99@users.noreply.github.com>
2022-12-03 23:05:31 +0100

    Dodaj pliki projektów.

---
+++ Input.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	public class Input
+	{
+		public Input(ILogger logger) 
+		{
+		 Logger = logger;
+		}
+		public ILogger Logger { get; set; }
+
+		public int PrintInputPairs(List<TimeData> dataStorage, TimeData inputPrevious)
+		{
+			int changes = 0;
+			Console.WriteLine(inputPrevious.ToString());
+			foreach (var inputCurrent in dataStorage.Skip(1))
+			{
+				if (inputCurrent.Message != inputPrevious.Message)
+				{
+					inputPrevious = inputCurrent;
+					Logger.Log(inputCurrent.ToString());
+					changes++;
+				}
+
+			}
+			return changes;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+using ConsoleApp1;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+var data = new List<TimeData>
+{
+	new TimeData("1615560000: 1"),
+	new TimeData("1615560005: 1"),
+	new TimeData("1615560013: 1"),
+	new TimeData("1615560018: 1"),
+	new TimeData("1615560024: 0"),
+	new TimeData("1615560030: 1"),
+	new TimeData("1615560037: 0"),
+	new TimeData("1615560042: 0")
+};
+
+data.OrderBy(x => x.Timestamp).ToList();
+TimeData inputPrevious;
+inputPrevious = data.First();
+new Input(new Logger()).PrintInputPairs(data, inputPrevious);
+
+
+public class TimeData
+{
+
+    public TimeData(string data)
+    {
+        var split = data.Split(':');
+		Timestamp = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(double.Parse(split.First()));
+        Message = int.Parse(split.Last().Trim()) == 1;
+	}
+    public DateTimeOffset Timestamp { get; private set; }
+
+    public bool Message { get; set; }
+
+	public override string ToString()
+	{
+        return Timestamp.ToUnixTimeSeconds().ToString() + ": " + (Message ? "1" : "0");
+	}
+
+}
